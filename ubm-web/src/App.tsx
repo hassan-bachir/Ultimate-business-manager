022c8233acdeb7162ff4e683d6ea155c05773789
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-06-29 22:11:41 +0300

    web: added the router and the routes

--- ubm-web/src/App.tsx
+++ ubm-web/src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
     return (
         <div className="App">
-            <h1>hello world!</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/Login" element={<LoginPage />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
