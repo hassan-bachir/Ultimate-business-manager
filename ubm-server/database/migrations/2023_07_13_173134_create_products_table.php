d9c78c33f2813b2c06596738bdba71a4482abf66
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-07-13 22:20:18 +0300

    server : migrated new table products

--- ubm-server/database/migrations/2023_07_13_173134_create_products_table.php
+++ ubm-server/database/migrations/2023_07_13_173134_create_products_table.php
@@ -12,7 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('product_id');
+            $table->string('product_name');
+            $table->text('product_description');
+            $table->string('product_part_number');
+            $table->string('product_make');
+            $table->string('product_manufacturer');
+            $table->integer('product_weight');
+            $table->string('product_fccid');
+            $table->float('product_cost');
+            $table->float('product_selling_price');
+            $table->string('product_condition');
+            $table->integer('product_quantity');
+            $table->string('product_storage');
+
             $table->timestamps();
         });
     }
